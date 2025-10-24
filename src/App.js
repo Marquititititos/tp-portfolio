714b3b6f1188002863be3fafaf63690f0c333810
Marquititititos <142502337+Marquititititos@users.noreply.github.com>
2025-10-24 09:09:24 -0300

    weuh

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="Arriba">
+        <div className="Nombre">
+          <span>Marcos</span>
+          <span>Tannenbaum</span>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
