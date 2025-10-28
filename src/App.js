01930988706bdeda2e2cdc61d1ddc200e1a5eb2e
Marquititititos <142502337+Marquititititos@users.noreply.github.com>
2025-10-28 10:36:27 -0300

    inicios pagina

--- src/App.js
+++ src/App.js
@@ -9,6 +9,20 @@ function App() {
           <span>Marcos</span>
           <span>Tannenbaum</span>
         </div>
+        <div className='Linea1'></div>
+        <div className='Atajos'>
+          <button className="botonMisProyectos" >Mis proyectos</button>
+          <button className="botonFormacion">Formación</button>
+          <button className="botonHabilidades">Habilidades</button>
+        </div>
+      </header>
+
+      <header className='Perfil'>
+        <div className='TextoPerfil'>
+           <span className='Titulo'>Desarrollador de videojuegos independiente</span>
+           <span className='Descripcion'>Estudiante de licenciatura en Tecnología Digital UTDT y tecnicatura en Programación Avanzada de Videojuegos.</span>
+        </div>
+        <image className='FotoPerfil'></image>
       </header>
     </div>
   );
