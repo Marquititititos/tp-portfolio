270f2a4a047f595ee59f3b2f18ac89c68fb6a469
Marquititititos <142502337+Marquititititos@users.noreply.github.com>
2025-10-31 09:05:26 -0300

    Formacion1

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 import fotoperfil from './images/image 1.png';
 import fotoRelleno from './images/image 2.png';
+import fotoDitella from './images/image 4.png';
+import fotoOrt from './images/image 5.png';
 
 function App() {
   return (
@@ -28,21 +30,41 @@ function App() {
       </header>
 
       <header className="misProyectos">
-        <span className="textoMisProyectos">Mis proyectos</span>
-        <div className="apocaliptic">
-          <img className="fotoApocaliptic" src={fotoRelleno}></img>
-          <div className="textoApocaliptic">
+        <span className="TituloElemento">Mis proyectos</span>
+        <div className="elementoIzquierda">
+          <img className="fotoIzquierda" src={fotoRelleno}></img>
+          <div className="textoIzquierda">
               <span className="subtitulo">ApocalipTIC</span>
-              <span className="descApocaliptic">Juego tower defense con kiajgoú wenrgjkn ber kweel bgflekwrbgi ewurbgke wwrjhbgk jebrgkjebgvkjljerbg kjhheww rfbgkljehrbg kherbge erlkeghrg lkerughek jlrhg luh bbgvkjw bgvkjbwrgkljw rbgkjwr  wñjjb rg.kjwbeeglkjwbgk ljwbgkjwbgkj webgkjbwkjbgj.</span>
+              <span className="descIzquierda">Juego tower defense con kiajgoú wenrgjkn ber kweel bgflekwrbgi ewurbgke wwrjhbgk jebrgkjebgvkjljerbg kjhheww rfbgkljehrbg kherbge erlkeghrg lkerughek jlrhg luh bbgvkjw bgvkjbwrgkljw rbgkjwr  wñjjb rg.kjwbeeglkjwbgk ljwbgkjwbgkj webgkjbwkjbgj.</span>
           </div>
         </div>
 
-        <div className="getOffMyTrain">
-          <div className="textoGetOffMyTrain">
-              <span className="tituloGetOffMyTrain">Get Off My Train</span>
-              <span className="descGetOffMyTrain">Juego tower defense con kiajgoú wenrgjkn ber kweel bgflekwrbgi ewurbgke wwrjhbgk jebrgkjebgvkjljerbg kjhheww rfbgkljehrbg kherbge erlkeghrg lkerughek jlrhg luh bbgvkjw bgvkjbwrgkljw rbgkjwr  wñjjb rg.kjwbeeglkjwbgk ljwbgkjwbgkj webgkjbwkjbgj.</span>
+        <div className="elementoDerecha">
+          <div className="textoDerecha">
+              <span className="tituloDerecha">Get Off My Train</span>
+              <span className="descDerecha">Juego tower defense con kiajgoú wenrgjkn ber kweel bgflekwrbgi ewurbgke wwrjhbgk jebrgkjebgvkjljerbg kjhheww rfbgkljehrbg kherbge erlkeghrg lkerughek jlrhg luh bbgvkjw bgvkjbwrgkljw rbgkjwr  wñjjb rg.kjwbeeglkjwbgk ljwbgkjwbgkj webgkjbwkjbgj.</span>
           </div>
-          <img className="fotoGetOffMyTrain" src={fotoRelleno}></img>
+          <img className="fotoDerecha" src={fotoRelleno}></img>
+        </div>
+      </header>
+
+      <header className="Formacion">
+         <span className="TituloElemento">Formación</span>
+
+         <div className="elementoIzquierda">
+          <img className="fotoIzquierda" src={fotoDitella}></img>
+          <div className="textoIzquierda">
+              <span className="subtitulo">Licenciatura en Tecnología Digital</span>
+              <span className="descIzquierda">Estoy estudiando en UTDT hace 2 años y me estoy formando en diversas tecnologías como C, C++, C#,  Python, JS, HTML, CSS. Además la formación se complementa con una persspectiva de emprendedurismo para prepararse en la cultura laboral empresarial.</span>
+          </div>
+        </div>
+
+        <div className="elementoDerecha">
+          <div className="textoDerecha">
+              <span className="tituloDerecha">Secundaria en Orientación TICs</span>
+              <span className="descDerecha">Texto que va a hacer MArcos Texto que va a hacer MArcos Texto que va a hacer MArcos Texto que va a hacer MArcos Texto que va a hacer MArcosTexto que va a hacer MArcosTexto que va a hacer MArcos</span>
+          </div>
+          <img className="fotoDerecha" src={fotoOrt}></img>
         </div>
       </header>
     </div>
