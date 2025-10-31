ad68ee4f40fe9ef8ddac77ac75faab2aee953982
Marquititititos <142502337+Marquititititos@users.noreply.github.com>
2025-10-31 08:51:51 -0300

    mis proyectos

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import fotoperfil from './images/image 1.png';
+import fotoRelleno from './images/image 2.png';
 
 function App() {
   return (
@@ -22,7 +24,26 @@ function App() {
            <span className='Titulo'>Desarrollador de videojuegos independiente</span>
            <span className='Descripcion'>Estudiante de licenciatura en Tecnología Digital UTDT y tecnicatura en Programación Avanzada de Videojuegos.</span>
         </div>
-        <image className='FotoPerfil'></image>
+        <img className='FotoPerfil' src={fotoperfil}></img>
+      </header>
+
+      <header className="misProyectos">
+        <span className="textoMisProyectos">Mis proyectos</span>
+        <div className="apocaliptic">
+          <img className="fotoApocaliptic" src={fotoRelleno}></img>
+          <div className="textoApocaliptic">
+              <span className="subtitulo">ApocalipTIC</span>
+              <span className="descApocaliptic">Juego tower defense con kiajgoú wenrgjkn ber kweel bgflekwrbgi ewurbgke wwrjhbgk jebrgkjebgvkjljerbg kjhheww rfbgkljehrbg kherbge erlkeghrg lkerughek jlrhg luh bbgvkjw bgvkjbwrgkljw rbgkjwr  wñjjb rg.kjwbeeglkjwbgk ljwbgkjwbgkj webgkjbwkjbgj.</span>
+          </div>
+        </div>
+
+        <div className="getOffMyTrain">
+          <div className="textoGetOffMyTrain">
+              <span className="tituloGetOffMyTrain">Get Off My Train</span>
+              <span className="descGetOffMyTrain">Juego tower defense con kiajgoú wenrgjkn ber kweel bgflekwrbgi ewurbgke wwrjhbgk jebrgkjebgvkjljerbg kjhheww rfbgkljehrbg kherbge erlkeghrg lkerughek jlrhg luh bbgvkjw bgvkjbwrgkljw rbgkjwr  wñjjb rg.kjwbeeglkjwbgk ljwbgkjwbgkj webgkjbwkjbgj.</span>
+          </div>
+          <img className="fotoGetOffMyTrain" src={fotoRelleno}></img>
+        </div>
       </header>
     </div>
   );
